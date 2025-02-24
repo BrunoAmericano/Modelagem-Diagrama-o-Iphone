1c949cb495ca4a11fa49f30466002a077236dd29
Bruno Prado <bruno.BJ684@gmail.com>
2025-02-24 14:42:10 +0000

    testando

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
+import iphone.iPhone; // Importando a classe do pacote correto
+
 public class Main {
     public static void main(String[] args) {
-        // Criando um objeto do iPhone
         iPhone iphone = new iPhone();
-        
-        // Testando as funcionalidades de Reprodutor Musical
+
         iphone.selecionarMusica("Shape of You");
         iphone.tocar();
         iphone.pausar();
-        
-        // Testando as funcionalidades de Aparelho Telefônico
+
         iphone.ligar("123456789");
         iphone.atender();
         iphone.iniciarCorreioVoz();
-        
-        // Testando as funcionalidades de Navegador na Internet
+
         iphone.exibirPagina("https://www.google.com");
         iphone.adicionarNovaAba();
         iphone.atualizarPagina();
--- src/iPhone.java
+++ src/iPhone.java
@@ -1,48 +1,38 @@
+package iphone; // Esta linha deve estar no topo do arquivo
+
 public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
-    
-    // Implementação do Reprodutor Musical
     @Override
     public void tocar() {
         System.out.println("Reproduzindo música...");
     }
-
     @Override
     public void pausar() {
         System.out.println("Música pausada.");
     }
-
     @Override
     public void selecionarMusica(String musica) {
         System.out.println("Selecionando a música: " + musica);
     }
-
-    // Implementação do Aparelho Telefônico
     @Override
     public void ligar(String numero) {
         System.out.println("Ligando para: " + numero);
     }
-
     @Override
     public void atender() {
         System.out.println("Atendendo a chamada...");
     }
-
     @Override
     public void iniciarCorreioVoz() {
         System.out.println("Iniciando correio de voz...");
     }
-
-    // Implementação do Navegador na Internet
     @Override
     public void exibirPagina(String url) {
         System.out.println("Exibindo página: " + url);
     }
-
     @Override
     public void adicionarNovaAba() {
         System.out.println("Abrindo nova aba...");
     }
-
     @Override
     public void atualizarPagina() {
         System.out.println("Atualizando página...");
